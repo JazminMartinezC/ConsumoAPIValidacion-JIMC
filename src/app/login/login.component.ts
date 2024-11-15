167f27bed372b90801fc62c2de16150674f14fb9
JazminMartinezC <21160705@itoaxaca.edu.mx>
2024-11-15 10:10:05 -0600

    Login con validaciones

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,67 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { CommonModule } from '@angular/common'; 
+import { UserService } from '../services/user.service';          // Importar el servicio
+import { MatSnackBar } from '@angular/material/snack-bar';
+import {MatStepperModule} from '@angular/material/stepper';
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    CommonModule,      
+    MatCardModule,
+    MatTabsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    FormsModule,
+    MatSnackBarModule, MatStepperModule
+  ],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  email: string = '';
+  password: string = '';
+  remail: string = '';
+  rpassword: string = '';
+  rconfirmPassword: string = '';
+
+  constructor(private router: Router, private userService: UserService, private snackBar: MatSnackBar) {}
+
+  iniciarPagina() {
+    // Validación de los campos de correo y contraseña
+    if (this.email && this.password) {
+      // Llamada al servicio para obtener los usuarios
+      this.userService.getUsers().subscribe(
+        (users) => {
+          // Buscar el usuario que coincida con el correo y la contraseña
+          const user = users.find(u => u.email === this.email && u.password === this.password);
+          
+          if (user) {
+            // Si el usuario es válido, navegar al dashboard
+            this.router.navigate(['/user-list']);
+          } else {
+            // Si no se encuentra un usuario que coincida, mostrar mensaje de error
+            this.snackBar.open('Credenciales incorrectas', 'Cerrar', { duration: 3000 });
+          }
+        },
+        (error) => {
+          // Manejo de error en caso de que falle la llamada a la API
+          console.error('Error al obtener los usuarios', error);
+          this.snackBar.open('Error al obtener los usuarios', 'Cerrar', { duration: 3000 });
+        }
+      );
+    } else {
+      // Si no se completan los campos
+      this.snackBar.open('Por favor complete los campos', 'Cerrar', { duration: 3000 });
+    }
+  }
+}
